d8fbde2457b111208864c8f96b9869059989fa43
EugeneViktP <popoff.zhenya@yandex.ru>
2025-04-26 19:42:18 +0300

    Refinement of the logic

--- src/main/java/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
+++ src/main/java/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
@@ -1,10 +1,10 @@
 package edu.javacourse.studentorder.validator.register;
 
+import edu.javacourse.studentorder.config.Config;
 import edu.javacourse.studentorder.domain.register.CityRegisterRequest;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.domain.Person;
 import edu.javacourse.studentorder.exception.CityRegisterException;
-import edu.javacourse.studentorder.exception.TransportException;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
@@ -13,15 +13,19 @@ import jakarta.ws.rs.core.MediaType;
 public class RealCityRegisterChecker implements CityRegisterChecker
 {
     public CityRegisterResponse checkPerson(Person person)
-            throws CityRegisterException, TransportException {
+            throws CityRegisterException {
 
+        try {
         CityRegisterRequest request = new CityRegisterRequest(person);
 
         Client client = ClientBuilder.newClient();
-        CityRegisterResponse response = client.target("http://localhost:8080/city-register-1.0/rest/show")
+        CityRegisterResponse response = client.target(Config.getProperty(Config.CR_URL))
                 .request(MediaType.APPLICATION_JSON)
                 .post(Entity.entity(request, MediaType.APPLICATION_JSON))
                 .readEntity(CityRegisterResponse.class);
         return response;
+        } catch (Exception ex) {
+            throw new CityRegisterException("1", ex.getMessage(), ex);
+        }
     }
 }
